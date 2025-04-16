bca042c5c17c737e69a5b662e5f2bfa7edd4a496
Steve Bilogan <steve.bilogan@gmail.com>
2025-04-16 14:58:32 -0400

    wip

--- sample/Platforms/WebAssembly/Program.cs
+++ sample/Platforms/WebAssembly/Program.cs
@@ -3,15 +3,15 @@ namespace WinUI.TableView.SampleApp;
 public class Program
 {
     private static App? _app;
-    public static int Main(string[] args)
-    {
-        Microsoft.UI.Xaml.Application.Start(_ => _app = new App());
-
-        return 0;
-    }
-    // public static async Task Main(string[] args)
+    // public static int Main(string[] args)
     // {
-    //         var host = new Uno.UI.Runtime.Skia.WebAssembly.Browser.WebAssemblyBrowserHost(() => _app = new App());
-	// 		await host.Run();
+    //     Microsoft.UI.Xaml.Application.Start(_ => _app = new App());
+
+    //     return 0;
     // }
+    public static async Task Main(string[] args)
+    {
+            var host = new Uno.UI.Runtime.Skia.WebAssembly.Browser.WebAssemblyBrowserHost(() => _app = new App());
+			await host.Run();
+    }
 }
